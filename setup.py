6d05cabf6b1e3e4ca27e5ae40a1400b5c5273698
fleishmang <fleishmang@janelia.hhmi.org>
2020-10-06 02:42:58 -0400

    bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="bigstream",
-    version="0.0.1",
+    version="0.0.2",
     author="Greg M. Fleishman",
     author_email="greg.nli10me@gmail.com",
     description="Tools for distributed alignment of massive images",
@@ -17,7 +17,7 @@ setuptools.setup(
         'opencv-python',
         'dask[complete]',
         'dask-jobqueue',
-
+        'greedypy',
         'zarr',
         'numcodecs',
     ]

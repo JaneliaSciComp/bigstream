b69b8e3ef3f46e5300929e36a8e7762eb256c321
fleishmang <fleishmang@janelia.hhmi.org>
2021-03-14 16:42:31 -0400

    v0.1.0 in progress storage

--- bigstream/affine.py
+++ bigstream/affine.py
@@ -4,53 +4,66 @@ from bigstream import ransac
 import dask.array as da
 from ClusterWrap.clusters import janelia_lsf_cluster
 
+WORKER_BUFFER = 4
 
-def dog_ransac_affine(
+
+def ransac_affine(
     fixed, moving,
     fixed_vox, moving_vox,
-    nspots,
-    cc_radius,
+    min_radius,
+    max_radius,
     match_threshold,
-    align_threshold,
+    cc_radius=12,
+    nspots=5000,
+    align_threshold=2.0,
+    num_sigma_max=10,
 ):
     """
     """
 
     # get spots
-    fixed_spots = features.dog_filter_3d(fixed, fixed_vox)
-    moving_spots = features.dog_filter_3d(moving, moving_vox)
-
-    # need a practical number of spots
-    spot_limit = 100000
-    if fixed_spots.shape[0] > spot_limit:
-        sort_idx = np.argsort(fixed_spots[:, 3])[::-1]
-        fixed_spots = fixed_spots[sort_idx, :][:spot_limit]
-    if moving_spots.shape[0] > spot_limit:
-        sort_idx = np.argsort(moving_spots[:, 3])[::-1]
-        moving_spots = moving_spots[sort_idx, :][:spot_limit]
-
-    # filter overlapping blobs
-    fixed_spots = features.prune_neighbors(fixed_spots)
-    moving_spots = features.prune_neighbors(moving_spots)
+    fixed_spots = features.blob_detection(
+        fixed, min_radius, max_radius,
+        num_sigma=max(max_radius-min_radius, num_sigma_max),
+        threshold=0, exclude_border=cc_radius,
+    )
+    moving_spots = features.blob_detection(
+        moving, min_radius, max_radius,
+        num_sigma=max(max_radius-min_radius, num_sigma_max),
+        threshold=0, exclude_border=cc_radius,
+    )
 
     # sort
     sort_idx = np.argsort(fixed_spots[:, 3])[::-1]
-    fixed_spots = fixed_spots[sort_idx, :][:nspots]
+    fixed_spots = fixed_spots[sort_idx, :3][:nspots]
     sort_idx = np.argsort(moving_spots[:, 3])[::-1]
-    moving_spots = moving_spots[sort_idx, :][:nspots]
+    moving_spots = moving_spots[sort_idx, :3][:nspots]
+
+    # convert to physical units
+    fixed_spots = fixed_spots * fixed_vox
+    moving_spots = moving_spots * moving_vox
 
     # get contexts
-    fixed_spots = features.get_all_context(fixed, fixed_spots, fixed_vox, cc_radius)
-    moving_spots = features.get_all_context(moving, moving_spots, moving_vox, cc_radius)
+    fixed_spots = features.get_spot_context(
+        fixed, fixed_spots, fixed_vox, cc_radius,
+    )
+    moving_spots = features.get_spot_context(
+        moving, moving_spots, moving_vox, cc_radius,
+    )
 
     # get point correspondences
-    correlations = ransac.pairwise_correlation(fixed_spots, moving_spots)
-    fixed_spots, moving_spots = ransac.match_points(
-        fixed_spots, moving_spots, correlations, match_threshold,
+    correlations = features.pairwise_correlation(
+        fixed_spots, moving_spots,
+    )
+    fixed_spots, moving_spots = features.match_points(
+        fixed_spots, moving_spots,
+        correlations, match_threshold,
     )
 
     # align
-    return ransac.ransac_align_points(fixed_spots, moving_spots, align_threshold)
+    return ransac.ransac_align_points(
+        fixed_spots, moving_spots, align_threshold,
+    )
 
 
 def interpolate_affines(affines):
@@ -61,15 +74,15 @@ def interpolate_affines(affines):
     return affines
 
 
-def dog_ransac_affine_distributed(
+def tiled_ransac_affine(
     fixed, moving,
     fixed_vox, moving_vox,
-    nspots,
-    cc_radius,
+    min_radius,
+    max_radius,
     match_threshold,
-    align_threshold,
-    blocksize=[256,]*3,
+    blocksize,
     cluster_kwargs={},
+    **kwargs,
 ):
     """
     """
@@ -77,27 +90,29 @@ def dog_ransac_affine_distributed(
     # get number of blocks required
     block_grid = np.ceil(np.array(fixed.shape) / blocksize).astype(int)
     nblocks = np.prod(block_grid)
+    overlap = [int(round(x/8)) for x in blocksize]
 
     # distributed computations done in cluster context
     with janelia_lsf_cluster(**cluster_kwargs) as cluster:
-        cluster.scale_cluster(nblocks)
+        cluster.scale_cluster(nblocks + WORKER_BUFFER)
 
         # wrap images as dask arrays
         fixed_da = da.from_array(fixed, chunks=blocksize)
         moving_da = da.from_array(moving, chunks=blocksize)
 
         # wrap affine function
-        def my_dog_ransac_affine(x, y):
-            affine = dog_ransac_affine(
+        def wrapped_ransac_affine(x, y):
+            affine = ransac_affine(
                 x, y, fixed_vox, moving_vox,
-                nspots, cc_radius, match_threshold, align_threshold,
+                min_radius, max_radius, match_threshold,
+                **kwargs,
             )
             return affine.reshape((1,1,1,3,4))
 
         # affine align all chunks
         affines = da.map_overlap(
-            my_dog_ransac_affine, fixed_da, moving_da,
-            depth=tuple([int(round(x/8)) for x in blocksize]),
+            wrapped_ransac_affine, fixed_da, moving_da,
+            depth=tuple(overlap),
             boundary='reflect',
             trim=False,
             align_arrays=False,
@@ -106,5 +121,17 @@ def dog_ransac_affine_distributed(
             chunks=[1, 1, 1, 3, 4],
         ).compute()
 
-    return interpolate_affines(affines)
+    # improve on identity affines
+    affines = interpolate_affines(affines)
+
+    # adjust affines for block origins
+    for i in range(nblocks):
+        x, y, z = np.unravel_index(i, block_grid)
+        origin = (np.array(blocksize) * [x, y, z] - overlap) * fixed_vox
+        tl, tr, a = np.eye(4), np.eye(4), np.eye(4)
+        tl[:3, -1], tr[:3, -1] = origin, -origin
+        a[:3, :] = affines[x, y, z]
+        affines[x, y, z] = np.matmul(tl, np.matmul(a, tr))[:3, :]
+
+    return affines
 
--- bigstream/features.py
+++ bigstream/features.py
@@ -1,97 +1,92 @@
 import numpy as np
-from scipy.ndimage import gaussian_filter, maximum_filter
-from scipy.spatial import cKDTree
-from skimage.feature.blob import _blob_overlap
+from fishspot.filter import white_tophat
+from fishspot.detect import detect_spots_log
 
 
-def difference_of_gaussians_3d(image, spacing, big_sigma, small_sigma):
-    """
-    """
-
-    spacing = np.array(spacing)
-    g1 = gaussian_filter(image, small_sigma/spacing)
-    g2 = gaussian_filter(image, big_sigma/spacing)
-    return g1 - g2
-
-
-def local_max_points(image, min_distance=3, threshold=None):
-    """
-    """
-
-    image_max = maximum_filter(image, min_distance)
-    if threshold is not None:
-        mask = np.logical_and(image == image_max, image >= threshold)
-    else:
-        mask = image == image_max
-    return np.column_stack(np.nonzero(mask))
-
-
-def dog_filter_3d(
+def blob_detection(
     image,
-    image_spacing,
-    big_sigma=6.0,
-    small_sigma=3.5,
-    threshold=2.0,
-    min_distance=5,
-    ):
+    min_blob_radius,
+    max_blob_radius,
+    **kwargs,
+):
     """
     """
 
-    # get dog locations with intensity greater than threshold
-    dog = difference_of_gaussians_3d(image, image_spacing, big_sigma, small_sigma)
-    coord = local_max_points(dog, min_distance=min_distance)
-    intensities = image[coord[:,0], coord[:,1], coord[:,2]]
-    filtered = intensities > threshold
-
-    # concat all results and return
-    coord = coord[filtered] * image_spacing  # points are in physical units
-    intensities = intensities[filtered][..., np.newaxis]
-    small_sigma_array = np.full((sum(filtered), 1), small_sigma)
-    big_sigma_array = np.full((sum(filtered), 1), big_sigma)
-    return np.hstack((coord, intensities, small_sigma_array, big_sigma_array))
+    wth = white_tophat(image, max_blob_radius)
+    spots = detect_spots_log(
+        wth,
+        min_blob_radius,
+        max_blob_radius,
+        **kwargs,
+    ).astype(int)
+    intensities = image[spots[:, 0], spots[:, 1], spots[:, 2]]
+    return np.hstack((spots[:, :3], intensities[..., None]))
 
 
-def prune_neighbors(spots, overlap=0.01, distance=6):
-    """
-    """
-
-    # get points within distance of each other
-    tree = cKDTree(spots[:, :-2])
-    pairs = np.array(list(tree.query_pairs(distance)))
-
-    # tag gaussian blobs that overlap too much
-    c = [0, 1, 2, 4]  # fancy index for coordinate + small_sigma
-    for (i, j) in pairs:
-        if _blob_overlap(spots[i, c], spots[j, c]) > overlap:
-            if spots[i, 3] > spots[j, 3]:
-                spots[j, 3] = 0
-            else:
-                spots[i, 3] = 0
-
-    # remove tagged spots and return as array
-    return np.array([s for s in spots if s[3] > 0])
-
-
-def get_context(image, position, radius):
-    """
-    """
-
-    p, r = np.round(position).astype(int), radius  # shorthand
-    w = image[p[0]-r:p[0]+r+1, p[1]-r:p[1]+r+1, p[2]-r:p[2]+r+1]
-    if np.product(w.shape) != (2*r+1)**3:  # just ignore near the edge
-        w = None
-    return w
-
-
-def get_all_context(image, spots, vox, radius):
+def get_spot_context(image, spots, vox, radius):
     """
     """
 
     output = []
     for spot in spots:
-        spot[:3] = spot[:3] / vox  # points were in physical units, need voxel units
-        context = get_context(image, spot, radius)
-        if context is not None:
-            output.append([spot, context])
+        s = (spot/vox).astype(int)
+        w = image[s[0]-radius:s[0]+radius+1,
+                  s[1]-radius:s[1]+radius+1,
+                  s[2]-radius:s[2]+radius+1]
+        output.append( [spot, w] )
     return output    
+
+
+def _stats(arr):
+    """
+    """
+
+    # compute mean and standard deviation along columns
+    arr = arr.astype(np.float64)
+    means = np.mean(arr, axis=1)
+    sqr_means = np.mean(np.square(arr), axis=1)
+    stddevs = np.sqrt( sqr_means - np.square(means) )
+    return means, stddevs
+
+
+def pairwise_correlation(A, B):
+    """
+    """
+
+    # grab and flatten context
+    a_con = np.array( [a[1].flatten() for a in A] )
+    b_con = np.array( [b[1].flatten() for b in B] )
+
+    # get means and std for all contexts, center contexts
+    a_mean, a_std = _stats(a_con)
+    b_mean, b_std = _stats(b_con)
+    a_con = a_con - a_mean[..., None]
+    b_con = b_con - b_mean[..., None]
+
+    # compute pairwise correlations
+    corr = np.matmul(a_con, b_con.T)
+    corr = corr / a_std[..., None]
+    corr = corr / b_std[None, ...]
+    corr = corr / a_con.shape[1]
+
+    # contexts with no variability are nan, set to 0
+    corr[np.isnan(corr)] = 0
+    return corr
+
+
+def match_points(A, B, scores, threshold):
+    """
+    """
+
+    # split positions from context
+    a_pos = np.array( [a[0] for a in A] )
+    b_pos = np.array( [b[0] for b in B] )
+
+    # get highest scores above threshold
+    best_indcs = np.argmax(scores, axis=1)
+    a_indcs = range(len(a_pos))
+    keeps = scores[(a_indcs, best_indcs)] > threshold
+
+    # return positions of corresponding points
+    return a_pos[keeps, :3], b_pos[best_indcs[keeps], :3]
 

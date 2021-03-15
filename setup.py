03a901cf526bd6b595643e8acf63e243da167e36
fleishmang <fleishmang@janelia.hhmi.org>
2021-03-14 23:27:41 -0400

    bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="bigstream",
-    version="0.0.3",
+    version="0.1.0b",
     author="Greg M. Fleishman",
     author_email="greg.nli10me@gmail.com",
     description="Tools for distributed alignment of massive images",

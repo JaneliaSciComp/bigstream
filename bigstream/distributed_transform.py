d74bf200b26ac17a6c1f1f084a466f6718ecae40
Cristian Goina <goinac@hhmi.org>
2023-09-11 15:33:45 -0400

    check point values shape

--- bigstream/distributed_transform.py
+++ bigstream/distributed_transform.py
@@ -402,7 +402,14 @@ def _transform_coords(block_index,
     warped_coord_indexed_values = np.empty_like(coord_indexed_values)
     warped_coord_indexed_values[:, 0:3] = warped_coords
     warped_coord_indexed_values[:, 3:] = points_values
-    return warped_coord_indexed_values
+    print(f'{time.ctime(time.time())} Finished block: ', block_index,
+          f'- warped {warped_coord_indexed_values.shape} coords',
+          flush=True)
+
+    if points_values.shape[1] == 0:
+        return warped_coords
+    else:
+        return warped_coord_indexed_values
 
 
 @cluster

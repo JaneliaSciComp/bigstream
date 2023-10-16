aff94abe3076dfd9c82fb457c4873beb7b247105
GFleishman <greg.nli10me@gmail.com>
2023-10-16 15:02:23 -0400

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="bigstream",
-    version="1.2.1",
+    version="1.2.2",
     author="Greg M. Fleishman",
     author_email="greg.nli10me@gmail.com",
     description="Tools for distributed alignment of massive images",

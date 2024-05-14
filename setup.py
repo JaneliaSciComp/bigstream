239c3c99e7d10bab491561e828304541d6f172d3
GFleishman <greg.nli10me@gmail.com>
2024-05-14 15:24:22 -0400

    bump version for bug fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="bigstream",
-    version="1.4.0",
+    version="1.4.1",
     author="Greg M. Fleishman",
     author_email="greg.nli10me@gmail.com",
     description="Tools for distributed alignment of massive images",

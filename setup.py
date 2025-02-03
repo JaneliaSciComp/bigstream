43f207ea231c721f9ef1a959531cba8ae12529ec
Cristian Goina <goinac@hhmi.org>
2025-02-03 12:27:22 -0500

    set the same version as it is in master

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="bigstream",
-    version="1.4.1",
+    version="4.0.0",
     author="Greg M. Fleishman",
     author_email="greg.nli10me@gmail.com",
     description="Tools for distributed alignment of massive images",

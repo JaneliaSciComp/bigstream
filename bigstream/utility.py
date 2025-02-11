9c4b5f220083e48c2ca58dae36b270b320e57fa9
Cristian Goina <goinac@hhmi.org>
2025-02-11 12:14:20 -0500

    pass SetSpacing param as a tuple

--- bigstream/utility.py
+++ bigstream/utility.py
@@ -77,7 +77,7 @@ def numpy_to_sitk(image, spacing=None, origin=None, vector=False):
     logger.info(f'Spacing used for {image.shape} image: {spacing} ({type(spacing)})')
     image = sitk.GetImageFromArray(image, isVector=vector)
     if spacing is None: spacing = np.ones(image.GetDimension())
-    image.SetSpacing(spacing[::-1])
+    image.SetSpacing(tuple(spacing[::-1]))
     if origin is None: origin = np.zeros(image.GetDimension())
     image.SetOrigin(origin[::-1])
     return image

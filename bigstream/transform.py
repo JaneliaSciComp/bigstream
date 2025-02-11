e6e5849b42b40f0c409d2f9089a8fe585c2f6d9c
Cristian Goina <goinac@hhmi.org>
2025-02-11 11:29:13 -0500

    fix syntax error - if shrink is None - handle it as 1

--- bigstream/transform.py
+++ bigstream/transform.py
@@ -568,6 +568,13 @@ def invert_displacement_vector_field(
             if np.any(pad):
                 pad_level = tuple(round(x*y) for x, y in zip(pad, shrink_tuple))
                 pad_crop = tuple(slice(x, -x) if x > 0 else slice(None) for x in pad_level)
+        else:
+            shrink_tuple = tuple(x for x in spacing) + (1,)
+            root_level = zoom(root_level, shrink_tuple,  mode='reflect', order=1)
+            spacing_level = np.array((1,) * len(spacing))
+            if np.any(pad):
+                pad_level = tuple(round(x*y) for x, y in zip(pad, shrink_tuple))
+                pad_crop = tuple(slice(x, -x) if x > 0 else slice(None) for x in pad_level)
 
         # initialize
         if level > 0:

03e713776d1c5ccd0eb1e3f316366fc7651e0095
Cristian Goina <goinac@janelia.hhmi.org>
2025-07-21 12:40:16 -0400

    bump up version - OME support added

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="bigstream",
-    version="5.0.2",
+    version="5.1.0",
     author="Greg M. Fleishman",
     author_email="greg.nli10me@gmail.com",
     description="Tools for distributed alignment of massive images",
